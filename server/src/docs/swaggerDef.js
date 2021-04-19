87ff59c0edc42318f51cb91c197aa2c00752a3ed
Tobias Rigler <8117@htl.rennweg.at>
2021-04-19 17:59:00 +0200

    Changed Swagger definitions

--- server/src/docs/swaggerDef.js
+++ server/src/docs/swaggerDef.js
@@ -4,12 +4,8 @@ const config = require('../config/config');
 const swaggerDef = {
   openapi: '3.0.0',
   info: {
-    title: 'node-express-boilerplate API documentation',
+    title: 'Helpupil API documentation',
     version,
-    license: {
-      name: 'MIT',
-      url: 'https://github.com/hagopj13/node-express-boilerplate/blob/master/LICENSE',
-    },
   },
   servers: [
     {
