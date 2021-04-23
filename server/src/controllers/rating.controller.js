d859c4c6f7ed584fb3a9ec8a3549b11c0f0baf3a
Tobias Rigler <8117@htl.rennweg.at>
2021-04-23 12:40:29 +0200

    Fixed rating bug

--- server/src/controllers/rating.controller.js
+++ server/src/controllers/rating.controller.js
@@ -5,8 +5,8 @@ const { moderatorService } = require('../services');
 const updateRating = catchAsync(async (req, res) => {
 	let document = await moderatorService.getDocumentById(req.params.documentId, req.body);
 
+    req.body.rating = (document.rating * document.reviewer.length + req.query.rating) / (document.reviewer.length + 1);
     document.reviewer = document.reviewer.push(req.user._id);
-    req.body.rating = (document.rating + req.query.rating) / (document.reviewer.length);
     req.body.reviewer = document.reviewer;
 
     document = await moderatorService.updateDocumentById(req.params.documentId, req.body);
