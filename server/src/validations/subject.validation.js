feaa11c7d096c183e6d8b6660b93ebf4b6fcff77
Anzola Fabio <anzola.fabio@gmail.com>
2021-04-23 20:40:25 +0200

    Added subject validation

---
+++ server/src/validations/subject.validation.js
@@ -0,0 +1,53 @@
+const Joi = require('joi');
+const { objectId } = require('./custom.validation');
+
+const createSubject = {
+  body: Joi.object().keys({
+    name: Joi.string().required(),
+    shortname: Joi.string().required(),
+    description: Joi.string().required(),
+  }),
+};
+
+const getSubjects = {
+  query: Joi.object().keys({
+    name: Joi.string(),
+    shortname: Joi.string(),
+    sortBy: Joi.string(),
+    limit: Joi.number().integer(),
+    page: Joi.number().integer(),
+  }),
+};
+
+const getSubject = {
+  params: Joi.object().keys({
+    subjectId: Joi.string().custom(objectId),
+  }),
+};
+
+const updateSubject = {
+  params: Joi.object().keys({
+    subjectId: Joi.required().custom(objectId),
+  }),
+  body: Joi.object()
+    .keys({
+      name: Joi.string().required(),
+    	shortname: Joi.string().required(),
+    	description: Joi.string().required(),
+    })
+    .min(1),
+};
+
+const deleteSubject = {
+  params: Joi.object().keys({
+    subjectId: Joi.string().custom(objectId),
+  }),
+};
+
+module.exports = {
+  createSubject,
+  getSubjects,
+	getSubject,
+	updateSubject,
+	deleteSubject,
+};
