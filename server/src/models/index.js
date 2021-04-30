821c15df3d5bbc0012013092c17db4d2417aad5e
Richard Krikler <8112@htl.rennweg.at>
2021-05-01 00:18:44 +0200

    Teacher model

--- server/src/models/index.js
+++ server/src/models/index.js
@@ -2,3 +2,4 @@ module.exports.Token = require('./token.model');
 module.exports.User = require('./user.model');
 module.exports.Doc = require('./document.model');
 module.exports.Subject = require('./subject.model');
+module.exports.Teacher = require('./teacher.model');
---
+++ server/src/models/teacher.model.js
@@ -0,0 +1,63 @@
+const mongoose = require('mongoose');
+const { toJSON, paginate } = require('./plugins');
+
+const teacherSchema = mongoose.Schema(
+	{
+		name: {
+			type: String,
+			required: true,
+			trim: true,
+		},
+		shortname: {
+			type: String,
+			required: true,
+			trim: true,
+		},
+		description: {
+			type: String,
+			required: true,
+			trim: true,
+		},
+		user: {
+			type: mongoose.SchemaTypes.ObjectId,
+			ref: 'User',
+			required: true,
+		},
+	},
+	{
+		timestamps: true,
+	}
+);
+
+// add plugin that converts mongoose to json
+teacherSchema.plugin(toJSON);
+teacherSchema.plugin(paginate);
+
+/**
+ * Check if name is taken
+ * @param {string} name - The teachers name
+ * @param {ObjectId} [excludeTeacherId] - The id of the teacher to be excluded
+ * @returns {Promise<boolean>}
+ */
+teacherSchema.statics.isNameTaken = async function (name, excludeTeacherId) {
+	const teacher = await this.findOne({ name, _id: { $ne: excludeTeacherId } });
+	return !!teacher;
+};
+
+/**
+ * Check if shortname is taken
+ * @param {string} name - The teachers shortname
+ * @param {ObjectId} [excludeTeacherId] - The id of the teacher to be excluded
+ * @returns {Promise<boolean>}
+ */
+teacherSchema.statics.isShortnameTaken = async function (shortname, excludeTeacherId) {
+	const teacher = await this.findOne({ shortname, _id: { $ne: excludeTeacherId } });
+	return !!teacher;
+};
+
+/**
+ * @typedef Teacher
+ */
+const Teacher = mongoose.model('Teacher', teacherSchema);
+
+module.exports = Teacher;
