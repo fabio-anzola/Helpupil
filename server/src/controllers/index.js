affd28768c5f051865ce000006f79d325a030e55
Richard Krikler <8112@htl.rennweg.at>
2021-04-30 23:46:50 +0200

    Teacher controller

--- server/src/controllers/index.js
+++ server/src/controllers/index.js
@@ -3,3 +3,4 @@ module.exports.userController = require('./user.controller');
 module.exports.documentController = require('./document.controller');
 module.exports.moderatorController = require('./moderator.controller');
 module.exports.subjectController = require('./subject.controller');
+module.exports.teacherController = require('./teacher.controller');
