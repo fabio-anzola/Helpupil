b5c374f8bdbfed124799db0efb7b4d3429c3965e
Anzola Fabio <anzola.fabio@gmail.com>
2021-05-01 21:36:46 +0200

    Added manageTeacher ability

--- server/src/config/roles.js
+++ server/src/config/roles.js
@@ -1,9 +1,9 @@
-const roles = ['user', 'admin', 'moderator'];
+const roles = ['user', 'moderator', 'admin'];
 
 const roleRights = new Map();
 roleRights.set(roles[0], []);
-roleRights.set(roles[1], ['getUsers', 'manageUsers']);
-roleRights.set(roles[2], ['getPendingDoc', 'manageDocs', 'manageSubjects']);
+roleRights.set(roles[1], ['getPendingDoc', 'manageDocs', 'manageSubjects', 'manageTeacher']);
+roleRights.set(roles[2], roleRights.get(roles[1]).concat(['getUsers', 'manageUsers']));
 
 module.exports = {
   roles,
