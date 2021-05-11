5d6ae829c0c6d941c559e8c4b4dfd17b8cb6df96
Richard Krikler <8112@htl.rennweg.at>
2021-05-11 17:45:23 +0200

    Added User response

---
+++ client/src/main/java/at/helpupil/application/utils/responses/User.java
@@ -0,0 +1,170 @@
+package at.helpupil.application.utils.responses;
+
+import java.util.ArrayList;
+
+public class User {
+    UserObj UserObject;
+    Tokens TokensObject;
+
+
+    // Getter Methods
+
+    public UserObj getUser() {
+        return UserObject;
+    }
+
+    public Tokens getTokens() {
+        return TokensObject;
+    }
+
+    // Setter Methods
+
+    public void setUser(UserObj userObject) {
+        this.UserObject = userObject;
+    }
+
+    public void setTokens(Tokens tokensObject) {
+        this.TokensObject = tokensObject;
+    }
+}
+
+class Tokens {
+    Access AccessObject;
+    Refresh RefreshObject;
+
+
+    // Getter Methods
+
+    public Access getAccess() {
+        return AccessObject;
+    }
+
+    public Refresh getRefresh() {
+        return RefreshObject;
+    }
+
+    // Setter Methods
+
+    public void setAccess(Access accessObject) {
+        this.AccessObject = accessObject;
+    }
+
+    public void setRefresh(Refresh refreshObject) {
+        this.RefreshObject = refreshObject;
+    }
+}
+
+class Refresh {
+    private String token;
+    private String expires;
+
+
+    // Getter Methods
+
+    public String getToken() {
+        return token;
+    }
+
+    public String getExpires() {
+        return expires;
+    }
+
+    // Setter Methods
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public void setExpires(String expires) {
+        this.expires = expires;
+    }
+}
+
+class Access {
+    private String token;
+    private String expires;
+
+
+    // Getter Methods
+
+    public String getToken() {
+        return token;
+    }
+
+    public String getExpires() {
+        return expires;
+    }
+
+    // Setter Methods
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public void setExpires(String expires) {
+        this.expires = expires;
+    }
+}
+
+class UserObj {
+    private String role;
+    private boolean isEmailVerified;
+    private float wallet;
+    ArrayList< Object > purchasedDocuments = new ArrayList < Object > ();
+    private String name;
+    private String email;
+    private String id;
+
+
+    // Getter Methods
+
+    public String getRole() {
+        return role;
+    }
+
+    public boolean getIsEmailVerified() {
+        return isEmailVerified;
+    }
+
+    public float getWallet() {
+        return wallet;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    // Setter Methods
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public void setIsEmailVerified(boolean isEmailVerified) {
+        this.isEmailVerified = isEmailVerified;
+    }
+
+    public void setWallet(float wallet) {
+        this.wallet = wallet;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
