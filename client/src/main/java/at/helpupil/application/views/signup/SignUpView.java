d8e247e8bc80d6fc5fe539d4e36d354648d49a9b
Tobias Rigler <8117@htl.rennweg.at>
2021-05-17 16:30:19 +0200

    Created Sign Up View
    ToDo: Sign Up Request

--- client/src/main/java/at/helpupil/application/views/signup/SignUpView.java
+++ client/src/main/java/at/helpupil/application/views/signup/SignUpView.java
@@ -1,20 +1,106 @@
 package at.helpupil.application.views.signup;
 
+import at.helpupil.application.utils.Auth;
 import at.helpupil.application.utils.OpenView;
+import at.helpupil.application.utils.SessionStorage;
+import at.helpupil.application.utils.requests.Login;
+import at.helpupil.application.utils.responses.Error;
+import at.helpupil.application.utils.responses.User;
 import at.helpupil.application.views.main.MainView;
-import com.vaadin.flow.component.Text;
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.Key;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.dependency.CssImport;
+import com.vaadin.flow.component.html.H3;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.EmailField;
+import com.vaadin.flow.component.textfield.PasswordField;
+import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.RouteAlias;
+import kong.unirest.HttpResponse;
+import kong.unirest.Unirest;
+
+import static at.helpupil.application.Application.BASE_URL;
 
 @Route(value = "signup", layout = MainView.class)
 @PageTitle("Sign Up")
-@CssImport("./views/signup/sign-up-view.css")
+@CssImport("./views/login/sign-up-view.css")
 public class SignUpView extends OpenView {
+
+    private EmailField email = new EmailField("Email address");
+    private TextField username = new TextField("Username: ");
+    private PasswordField password = new PasswordField("Password");
+
+    private Button clear = new Button("Clear");
+    private Button signUp = new Button("Sign Up");
+
 
     public SignUpView() {
         addClassName("sign-up-view");
-        add(new Text("Content placeholder"));
+
+        add(createTitle());
+        add(createFormLayout());
+        add(createButtonLayout());
+
+        clearForm();
+
+        clear.addClickListener(e -> clearForm());
+        signUp.addClickListener(e -> {
+            if (!email.getValue().trim().isEmpty() && !email.isInvalid() && !username.getValue().trim().isEmpty() && !password.getValue().trim().isEmpty()) {
+                makeSignUpRequest(email.getValue().trim(), username.getValue().trim(), password.getValue().trim());
+                clearForm();
+            } else {
+                Notification.show("Check your input");
+            }
+        });
     }
 
+    private void clearForm() {
+        email.setValue("");
+        username.setValue("");
+        password.setValue("");
+    }
+
+    private Component createTitle() {
+        return new H3("Sign Up");
+    }
+
+    private Component createFormLayout() {
+        VerticalLayout formLayout = new VerticalLayout();
+        formLayout.addClassName("form-layout");
+        email.setErrorMessage("Please enter a valid email address");
+        formLayout.add(email, username, password);
+        return formLayout;
+    }
+
+    private Component createButtonLayout() {
+        HorizontalLayout buttonLayout = new HorizontalLayout();
+        buttonLayout.addClassName("button-layout");
+        signUp.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+        signUp.addClickShortcut(Key.ENTER);
+        buttonLayout.add(signUp);
+        buttonLayout.add(clear);
+        return buttonLayout;
+    }
+
+    private void makeSignUpRequest(String email, String username, String password) {
+/*        HttpResponse<User> user = Unirest.post(BASE_URL + "/auth/login")
+                .contentType("application/json")
+                .body(new Login(email, password))
+                .asObject(User.class);
+
+        Error error = user.mapError(Error.class);
+
+        if (null == error) {
+            SessionStorage.set(user.getBody());
+            Auth.redirectIfValid();
+        } else {
+            Notification.show("Check your Credentials");
+        }*/
+    }
 }
