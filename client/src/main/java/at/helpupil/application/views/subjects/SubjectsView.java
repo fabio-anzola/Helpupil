336120ddc03d4703499c38c18f3d1e5f30b65bf1
Anzola Fabio <anzola.fabio@gmail.com>
2021-05-17 23:43:19 +0200

    Subject card view

--- client/src/main/java/at/helpupil/application/views/subjects/SubjectsView.java
+++ client/src/main/java/at/helpupil/application/views/subjects/SubjectsView.java
@@ -1,11 +1,24 @@
 package at.helpupil.application.views.subjects;
 
 import at.helpupil.application.utils.SecuredView;
+import at.helpupil.application.utils.SessionStorage;
+import at.helpupil.application.utils.responses.Error;
+import at.helpupil.application.utils.responses.Subject;
+import at.helpupil.application.utils.responses.Subjects;
 import at.helpupil.application.views.main.MainView;
-import com.vaadin.flow.component.Text;
+import com.github.appreciated.card.Card;
+import com.github.appreciated.card.label.PrimaryLabel;
+import com.github.appreciated.card.label.SecondaryLabel;
+import com.github.appreciated.card.label.TitleLabel;
 import com.vaadin.flow.component.dependency.CssImport;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import kong.unirest.HttpResponse;
+import kong.unirest.Unirest;
+
+import static at.helpupil.application.Application.BASE_URL;
 
 @Route(value = "subjects", layout = MainView.class)
 @PageTitle("Subjects")
@@ -14,7 +27,38 @@ public class SubjectsView extends SecuredView {
 
     public SubjectsView() {
         addClassName("subjects-view");
-        add(new Text("Content placeholder"));
+
+        HorizontalLayout layout = new HorizontalLayout();
+        layout.setWidth("100%");
+        layout.setSpacing(true);
+        Subjects subject = getSubjects(1);
+        for (Subject result : subject.getResults()) {
+            Card card = new Card(
+                    new TitleLabel(result.getName()),
+                    new PrimaryLabel(result.getShortname()),
+                    new SecondaryLabel(result.getDescription())
+            );
+            card.setWidth("250px");
+            layout.add(card);
+        }
+
+        add(layout);
+    }
+
+    private Subjects getSubjects(int page) {
+        HttpResponse<Subjects> subjects = Unirest.get(BASE_URL + "/subject")
+                .queryString("page", page)
+                .header("Authorization", "Bearer " + SessionStorage.get().getTokens().getAccess().getToken())
+                .asObject(Subjects.class);
+
+        Error error = subjects.mapError(Error.class);
+
+        if (null == error) {
+            return subjects.getBody();
+        } else {
+            Notification.show(error.getMessage());
+        }
+        return null;
     }
 
 }
