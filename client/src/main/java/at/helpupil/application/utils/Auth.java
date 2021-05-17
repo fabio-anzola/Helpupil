c511b12ab914b703c38de13350bb081120fddb1f
Anzola Fabio <anzola.fabio@gmail.com>
2021-05-17 16:12:41 +0200

    Redirects via js

--- client/src/main/java/at/helpupil/application/utils/Auth.java
+++ client/src/main/java/at/helpupil/application/utils/Auth.java
@@ -1,20 +1,28 @@
 package at.helpupil.application.utils;
 
 import com.vaadin.flow.component.UI;
+import com.vaadin.flow.server.VaadinService;
+import com.vaadin.flow.server.VaadinServletRequest;
+
+import javax.servlet.http.HttpServletRequest;
 
 public class Auth {
 
     public static void redirectIfNotValid() {
         if (SessionStorage.isNull()) {
-            UI.getCurrent().navigate("login");
-            UI.getCurrent().getPage().reload();
+            HttpServletRequest httpServletRequest = ((VaadinServletRequest) VaadinService.getCurrentRequest()).getHttpServletRequest();
+            String[] requestUrl = httpServletRequest.getRequestURL().toString().split("/");
+            String req = requestUrl[0] + "//" + requestUrl[2];
+            UI.getCurrent().getPage().executeJs("window.location = \"" + req + "/login\"");
         }
     }
 
     public static void redirectIfValid() {
         if (!SessionStorage.isNull()) {
-            UI.getCurrent().navigate("documents");
-            UI.getCurrent().getPage().reload();
+            HttpServletRequest httpServletRequest = ((VaadinServletRequest) VaadinService.getCurrentRequest()).getHttpServletRequest();
+            String[] requestUrl = httpServletRequest.getRequestURL().toString().split("/");
+            String req = requestUrl[0] + "//" + requestUrl[2];
+            UI.getCurrent().getPage().executeJs("window.location = \"" + req + "/documents\"");
         }
     }
 
