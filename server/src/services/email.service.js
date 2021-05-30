542ec845f691c2734d4f85b61a5868d14d42f40c
Anzola Fabio <anzola.fabio@gmail.com>
2021-05-30 15:01:53 +0200

    Reformat verify mail

--- server/src/services/email.service.js
+++ server/src/services/email.service.js
@@ -29,11 +29,11 @@ const sendEmail = async (to, subject, text) => {
  * @param {string} token
  * @returns {Promise}
  */
-const sendResetPasswordEmail = async (to, token) => {
+const sendResetPasswordEmail = async (to, name, token) => {
   const subject = 'Reset password';
   // replace this url with the link to the reset password page of your front-end app
   const resetPasswordUrl = `https://mail.helpupil.at/?type=reset&token=${token}`;
-  const text = `Dear user,
+  const text = `Hi ${name},
 To reset your password, click on this link: ${resetPasswordUrl}
 If you did not request any password resets, then ignore this email.`;
   await sendEmail(to, subject, text);
@@ -45,13 +45,16 @@ If you did not request any password resets, then ignore this email.`;
  * @param {string} token
  * @returns {Promise}
  */
-const sendVerificationEmail = async (to, token) => {
+const sendVerificationEmail = async (to, name, token) => {
   const subject = 'Email Verification';
   // replace this url with the link to the email verification page of your front-end app
   const verificationEmailUrl = `https://mail.helpupil.at/?type=verify&token=${token}`;
-  const text = `Dear user,
-To verify your email, click on this link: ${verificationEmailUrl}
-If you did not create an account, then ignore this email.`;
+  const text = `Hi ${name},
+To verify your email, click on this link: 
+${verificationEmailUrl}
+
+If you did not create an account, then ignore this email.
+Your Helpupil Team`;
   await sendEmail(to, subject, text);
 };
 
