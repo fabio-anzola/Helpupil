018853b78ee488c50f5594a4305c357266d32467
Tobias Rigler <8117@htl.rennweg.at>
2021-06-04 09:25:58 +0200

    StarObj extends Icon and has index

--- client/src/main/java/at/helpupil/application/utils/StarObj.java
+++ client/src/main/java/at/helpupil/application/utils/StarObj.java
@@ -1,4 +1,26 @@
 package at.helpupil.application.utils;
 
-public class StarObj {
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.icon.VaadinIcon;
+
+public class StarObj extends Icon {
+    private int index;
+
+    public StarObj(VaadinIcon icon, int index) {
+        super(icon);
+        this.index = index;
+    }
+
+    public StarObj(String icon, int index) {
+        super(icon);
+        this.index = index;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public void setIndex(int index) {
+        this.index = index;
+    }
 }
