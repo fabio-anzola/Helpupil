166c861de1a0d6e6bdc69b6d0a9482c342cb37eb
Fabio Anzola <anzola.fabio@gmail.com>
2021-06-06 18:25:02 +0200

    calling correct functions on parameter set

--- client/src/main/java/at/helpupil/application/views/documents/DocumentsView.java
+++ client/src/main/java/at/helpupil/application/views/documents/DocumentsView.java
@@ -32,6 +32,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import static at.helpupil.application.Application.BASE_URL;
+import static at.helpupil.application.utils.Resolve.*;
 
 @Route(value = "documents", layout = MainView.class)
 @PageTitle("Documents")
@@ -409,84 +410,14 @@ public class DocumentsView extends SecuredView implements HasUrlParameter<String
         return null;
     }
 
-    private String resolveSubjectByShortname(String shortname) {
-        HttpResponse<Subject> subject = Unirest.get(BASE_URL + "/subject")
-                .header("Authorization", "Bearer " + SessionStorage.get().getTokens().getAccess().getToken())
-                .queryString("shortname", shortname)
-                .asObject(Subject.class);
-
-        Error error = subject.mapError(Error.class);
-
-        if (null == error) {
-            return subject.getBody().getId();
-        } else {
-            return shortname;
-        }
-    }
-
-    private String resolveTeacherByShortname(String shortname) {
-        HttpResponse<Teacher> teacher = Unirest.get(BASE_URL + "/teacher")
-                .header("Authorization", "Bearer " + SessionStorage.get().getTokens().getAccess().getToken())
-                .queryString("shortname", shortname)
-                .asObject(Teacher.class);
-
-        Error error = teacher.mapError(Error.class);
-
-        if (null == error) {
-            return teacher.getBody().getId();
-        } else {
-            return shortname;
-        }
-    }
-
-    private String resolveSubjectById(String id) {
-        HttpResponse<Subject> subject = Unirest.get(BASE_URL + "/subject/" + id)
-                .header("Authorization", "Bearer " + SessionStorage.get().getTokens().getAccess().getToken())
-                .asObject(Subject.class);
-
-        Error error = subject.mapError(Error.class);
-
-        if (null == error) {
-            return subject.getBody().getShortname();
-        } else {
-            return id;
-        }
-    }
-
-    private String resolveTeacherById(String id) {
-        HttpResponse<Teacher> teacher = Unirest.get(BASE_URL + "/teacher/" + id)
-                .header("Authorization", "Bearer " + SessionStorage.get().getTokens().getAccess().getToken())
-                .asObject(Teacher.class);
-
-        Error error = teacher.mapError(Error.class);
-
-        if (null == error) {
-            return teacher.getBody().getShortname();
-        } else {
-            return id;
-        }
-    }
-
-    private String resolveUserById(String id) {
-        HttpResponse<UserPublicObj> user = Unirest.get(BASE_URL + "/users/public/" + id)
-                .header("Authorization", "Bearer " + SessionStorage.get().getTokens().getAccess().getToken())
-                .asObject(UserPublicObj.class);
-
-        Error error = user.mapError(Error.class);
-
-        if (null == error) {
-            return user.getBody().getName();
-        } else {
-            return id;
-        }
-    }
-
     @Override
     public void setParameter(BeforeEvent beforeEvent, @WildcardParameter String s) {
-        if (s.isEmpty()) {
-            setText("Welcome anonymous.");
+        if (!s.isEmpty() && s.split("/").length == 2) {
+            this.documents = getDocuments(this.limit, 1, s.split("/")[0], s.split("/")[1]);
+            System.err.println("Got parameters");
         } else {
-            setText(String.format("Handling parameter %s.", s));
+            this.documents = getDocuments(this.limit, 1);
+            System.err.println("Got NO parameters");
         }
     }
 }
