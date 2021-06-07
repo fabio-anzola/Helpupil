dee33116ba2d3ec6cdeb95dc98e30151f7f5e714
Fabio Anzola <anzola.fabio@gmail.com>
2021-06-07 20:31:10 +0200

    Enabled swagger for production

--- server/src/docs/swaggerDef.js
+++ server/src/docs/swaggerDef.js
@@ -9,7 +9,7 @@ const swaggerDef = {
   },
   servers: [
     {
-      url: `http://localhost:${config.port}/v1`,
+      url: `${config.base}v1`,
     },
   ],
 };
--- server/src/routes/v1/index.js
+++ server/src/routes/v1/index.js
@@ -44,26 +44,30 @@ const defaultRoutes = [
   {
     path: '/content',
     route: contentRoute,
-  }
-];
-
-const devRoutes = [
-  // routes available only in development mode
+  },
   {
     path: '/docs',
     route: docsRoute,
   },
 ];
 
+// const devRoutes = [
+//   // routes available only in development mode
+//   {
+//     path: '/docs',
+//     route: docsRoute,
+//   },
+// ];
+
 defaultRoutes.forEach((route) => {
   router.use(route.path, route.route);
 });
 
-/* istanbul ignore next */
-if (config.env === 'development') {
-  devRoutes.forEach((route) => {
-    router.use(route.path, route.route);
-  });
-}
+// /* istanbul ignore next */
+// if (config.env === 'development') {
+//   devRoutes.forEach((route) => {
+//     router.use(route.path, route.route);
+//   });
+// }
 
 module.exports = router;
