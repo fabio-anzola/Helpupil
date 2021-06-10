65204b191b7e01cb7e7005c6944b670a4ae42945
Tobias Rigler <8117@htl.rennweg.at>
2021-06-10 18:58:31 +0200

    Updated LeaderboardObj and LeaderboadView

--- client/src/main/java/at/helpupil/application/utils/responses/LeaderboardObj.java
+++ client/src/main/java/at/helpupil/application/utils/responses/LeaderboardObj.java
@@ -1,2 +1,27 @@
-package at.helpupil.application.utils.responses;public class LeaderboardObj {
+package at.helpupil.application.utils.responses;
+
+public class LeaderboardObj {
+    String username;
+    int wallet;
+
+    public LeaderboardObj(String username, int wallet) {
+        this.username = username;
+        this.wallet = wallet;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public int getWallet() {
+        return wallet;
+    }
+
+    public void setWallet(int wallet) {
+        this.wallet = wallet;
+    }
 }
--- client/src/main/java/at/helpupil/application/views/leaderboard/LeaderboardView.java
+++ client/src/main/java/at/helpupil/application/views/leaderboard/LeaderboardView.java
@@ -1,10 +1,18 @@
 package at.helpupil.application.views.leaderboard;
 
 import at.helpupil.application.utils.SecuredView;
+import at.helpupil.application.utils.SessionStorage;
+import at.helpupil.application.utils.responses.Error;
+import at.helpupil.application.utils.responses.LeaderboardObj;
 import at.helpupil.application.views.main.MainView;
 import com.vaadin.flow.component.dependency.CssImport;
+import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import kong.unirest.HttpResponse;
+import kong.unirest.Unirest;
+
+import static at.helpupil.application.Application.BASE_URL;
 
 @Route(value = "leaderboard", layout = MainView.class)
 @PageTitle("Leaderboard")
@@ -12,6 +20,21 @@ import com.vaadin.flow.router.Route;
 public class LeaderboardView extends SecuredView {
     public LeaderboardView() {
         addClassName("leaderboard-view");
-        
+
+    }
+
+    private LeaderboardObj makeLeaderboardRequest() {
+        HttpResponse<LeaderboardObj> topUsers = Unirest.get(BASE_URL + "/leaderboard")
+                .header("Authorization", "Bearer " + SessionStorage.get().getTokens().getAccess().getToken())
+                .asObject(LeaderboardObj.class);
+
+        Error error = topUsers.mapError(Error.class);
+
+        if (null == error) {
+            return topUsers.getBody();
+        } else {
+            Notification.show(error.getMessage());
+        }
+        return null;
     }
 }
