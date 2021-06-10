94422db1a732413218c6292293590f30673cc27f
Anzola Fabio <anzola.fabio@gmail.com>
2021-06-10 18:17:04 +0200

    Add uploaded document to self when approved

--- server/src/controllers/moderator.controller.js
+++ server/src/controllers/moderator.controller.js
@@ -16,7 +16,10 @@ const getDocuments = catchAsync(async (req, res) => {
 const approve = catchAsync(async (req, res) => {
 	req.body.status = statusTypes.APPROVED;
 	const document = await moderatorService.updateDocumentById(req.params.documentId, req.body);
-	const user = await userService.addCoins(document.user, priceTypes[document.type.toUpperCase()]);
+	await userService.addCoins(document.user, priceTypes[document.type.toUpperCase()]);
+	const user = await userService.getUserById(document.user);
+	user.purchasedDocuments.push(document._id);
+	await userService.updateUserById(document.user, user);
 	res.send(document);
 });
 
