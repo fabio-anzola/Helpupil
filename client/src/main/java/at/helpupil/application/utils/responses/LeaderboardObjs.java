af8f859f51acc52095f652b0b5cfc6c4e591e205
Tobias Rigler <8117@htl.rennweg.at>
2021-06-10 20:12:28 +0200

    Updated LeaderboardObjs

--- client/src/main/java/at/helpupil/application/utils/responses/LeaderboardObjs.java
+++ client/src/main/java/at/helpupil/application/utils/responses/LeaderboardObjs.java
@@ -1,2 +1,17 @@
-package at.helpupil.application.utils.responses;public class LeaderboardObjs {
+package at.helpupil.application.utils.responses;
+
+public class LeaderboardObjs {
+    private LeaderboardObj[] results;
+
+    public LeaderboardObjs(LeaderboardObj[] results) {
+        this.results = results;
+    }
+
+    public LeaderboardObj[] getResults() {
+        return results;
+    }
+
+    public void setResults(LeaderboardObj[] results) {
+        this.results = results;
+    }
 }
