ece39fd9987bcbdb38bda884b393a57f26aa5c3b
Anzola Fabio <anzola.fabio@gmail.com>
2021-06-11 10:32:04 +0200

    Usage of document service from moderator controller

--- server/src/controllers/moderator.controller.js
+++ server/src/controllers/moderator.controller.js
@@ -2,14 +2,14 @@ const httpStatus = require('http-status');
 const pick = require('../utils/pick');
 const ApiError = require('../utils/ApiError');
 const catchAsync = require('../utils/catchAsync');
-const { moderatorService, userService } = require('../services');
+const { documentService, moderatorService, userService } = require('../services');
 const { statusTypes, priceTypes, documentTypes } = require('../config/documents');
 
 const getDocuments = catchAsync(async (req, res) => {
 	const filter = pick(req.query, ['name', 'type']);
 	filter.status = statusTypes.PENDING;
 	const options = pick(req.query, ['sortBy', 'limit', 'page']);
-	const result = await moderatorService.queryDocuments(filter, options);
+	const result = await documentService.queryDocuments(filter, options);
 	res.send(result);
 });
 
