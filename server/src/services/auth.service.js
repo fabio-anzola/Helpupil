660d65072b216dc71107b09114db7de323273263
Anzola Fabio <anzola.fabio@gmail.com>
2021-06-11 12:19:25 +0200

    Allow a message when approve and decline

--- server/src/services/auth.service.js
+++ server/src/services/auth.service.js
@@ -1,6 +1,7 @@
 const httpStatus = require('http-status');
 const tokenService = require('./token.service');
 const userService = require('./user.service');
+const emailService = require('./email.service');
 const Token = require('../models/token.model');
 const ApiError = require('../utils/ApiError');
 const { tokenTypes } = require('../config/tokens');
