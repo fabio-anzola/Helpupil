4ae0e51058a0d67aa5580014cbe3aa12f8fb773e
Tobias Rigler <8117@htl.rennweg.at>
2021-06-13 22:27:34 +0200

    Added JavaDoc for Refresh

--- client/src/main/java/at/helpupil/application/utils/responses/Refresh.java
+++ client/src/main/java/at/helpupil/application/utils/responses/Refresh.java
@@ -1,26 +1,51 @@
 package at.helpupil.application.utils.responses;
 
+/**
+ * Response object of Refresh
+ */
 public class Refresh {
+    /**
+     * refresh token to get new access token
+     */
     private String token;
+    /**
+     * date when refresh token expires
+     */
     private String expires;
 
+    /**
+     * @param token refresh token
+     * @param expires date when token expires
+     */
     public Refresh(String token, String expires) {
         this.token = token;
         this.expires = expires;
     }
 
+    /**
+     * @return refresh token
+     */
     public String getToken() {
         return token;
     }
 
+    /**
+     * @return date when token expires
+     */
     public String getExpires() {
         return expires;
     }
 
+    /**
+     * @param token refresh token
+     */
     public void setToken(String token) {
         this.token = token;
     }
 
+    /**
+     * @param expires date when refresh token expires
+     */
     public void setExpires(String expires) {
         this.expires = expires;
     }
