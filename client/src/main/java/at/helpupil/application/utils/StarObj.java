eade80717573e4ae3774b4a1a3508834d73ea5b3
Tobias Rigler <8117@htl.rennweg.at>
2021-06-13 23:20:31 +0200

    Added JavaDoc for StarObj

--- client/src/main/java/at/helpupil/application/utils/StarObj.java
+++ client/src/main/java/at/helpupil/application/utils/StarObj.java
@@ -3,14 +3,26 @@ package at.helpupil.application.utils;
 import com.vaadin.flow.component.icon.Icon;
 import com.vaadin.flow.component.icon.VaadinIcon;
 
+/**
+ * Star icons for rating
+ */
 public class StarObj extends Icon {
+    /**
+     * star is filled, if state is true
+     */
     private final boolean state;
 
+    /**
+     * @param state of star, true equals filled
+     */
     public StarObj(boolean state) {
         super(state ? VaadinIcon.STAR : VaadinIcon.STAR_O);
         this.state = state;
     }
 
+    /**
+     * @return true if star is filled
+     */
     public boolean getState() {
         return state;
     }
