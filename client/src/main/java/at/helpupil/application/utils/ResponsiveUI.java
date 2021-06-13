671a0f97805df4062de1209d4b1fb4f5b2fb4389
Richard Krikler <8112@htl.rennweg.at>
2021-06-14 00:13:14 +0200

    Check if width == 0 -> LayoutMode = Desktop

--- client/src/main/java/at/helpupil/application/utils/ResponsiveUI.java
+++ client/src/main/java/at/helpupil/application/utils/ResponsiveUI.java
@@ -20,7 +20,8 @@ public class ResponsiveUI {
     public static LayoutMode getLayoutMode() {
         int[] windowWidth = new int[1];
         UI.getCurrent().getPage().retrieveExtendedClientDetails(details -> windowWidth[0] = details.getWindowInnerWidth());
-        if (windowWidth[0] < 900) {
+
+        if (windowWidth[0] != 0 && windowWidth[0] < 900) {
             return LayoutMode.SMALL;
         } else {
             return LayoutMode.DESKTOP;
