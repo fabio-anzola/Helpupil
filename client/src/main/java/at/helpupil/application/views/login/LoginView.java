508a3df93b6ba719a094b322ddbdfd4e037d298b
Tobias Rigler <8117@htl.rennweg.at>
2021-06-14 00:33:15 +0200

    Added JavaDoc for LoginView

--- client/src/main/java/at/helpupil/application/views/login/LoginView.java
+++ client/src/main/java/at/helpupil/application/views/login/LoginView.java
@@ -28,20 +28,41 @@ import kong.unirest.Unirest;
 
 import static at.helpupil.application.Application.BASE_URL;
 
+/**
+ * This view is shown to the user so he can log himself in
+ */
 @Route(value = "login", layout = MainView.class)
 @RouteAlias(value = "", layout = MainView.class) //This is responsible for this page being default
 @PageTitle("Login")
 @CssImport("./views/login/login-view.css")
 public class LoginView extends OpenView {
 
+    /**
+     * field to enter email
+     */
     private final EmailField email = new EmailField("Email address");
+    /**
+     * field to enter password
+     */
     private final PasswordField password = new PasswordField("Password");
+    /**
+     * button to reset password
+     */
     private final Button forgotPassword = new Button("Forgot Password");
 
+    /**
+     * button to clear fields
+     */
     private final Button clear = new Button("Clear");
+    /**
+     * log into account
+     */
     private final Button login = new Button("Login");
 
 
+    /**
+     * initializes view
+     */
     public LoginView() {
         ThemeHelper.onLoad();
 
@@ -64,15 +85,24 @@ public class LoginView extends OpenView {
         forgotPassword.addClickListener(e -> forgotPassword(email.getValue().trim()));
     }
 
+    /**
+     * clears form fields
+     */
     private void clearForm() {
         email.setValue("");
         password.setValue("");
     }
 
+    /**
+     * @return title as H3
+     */
     private Component createTitle() {
         return new H3("Login");
     }
 
+    /**
+     * @return layout for the form
+     */
     private Component createFormLayout() {
         VerticalLayout formLayout = new VerticalLayout();
         formLayout.addClassName("form-layout");
@@ -82,6 +112,9 @@ public class LoginView extends OpenView {
         return formLayout;
     }
 
+    /**
+     * @return layout for the buttons
+     */
     private Component createButtonLayout() {
         HorizontalLayout buttonLayout = new HorizontalLayout();
         buttonLayout.addClassName("button-layout");
@@ -92,6 +125,11 @@ public class LoginView extends OpenView {
         return buttonLayout;
     }
 
+    /**
+     * makes request to api to log into an account
+     * @param email of user
+     * @param password of user
+     */
     private void makeLoginRequest(String email, String password) {
         HttpResponse<User> user = Unirest.post(BASE_URL + "/auth/login")
                 .contentType("application/json")
@@ -108,6 +146,10 @@ public class LoginView extends OpenView {
         }
     }
 
+    /**
+     * makes api request so user gets an email to reset his password
+     * @param email of user who forgot his password
+     */
     private void forgotPassword(String email) {
         Unirest.post(BASE_URL + "/auth/forgot-password")
                 .contentType("application/json")
