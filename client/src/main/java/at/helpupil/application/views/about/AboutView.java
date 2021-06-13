5200b5789c96db52ef88d3574d53c1d830177fd4
Anzola Fabio <anzola.fabio@gmail.com>
2021-06-13 18:29:46 +0200

    Added url parser

--- client/src/main/java/at/helpupil/application/views/about/AboutView.java
+++ client/src/main/java/at/helpupil/application/views/about/AboutView.java
@@ -7,17 +7,39 @@ import com.vaadin.flow.router.Route;
 import com.vaadin.flow.router.PageTitle;
 import at.helpupil.application.views.main.MainView;
 import com.vaadin.flow.component.dependency.CssImport;
+import org.commonmark.node.Node;
+import org.commonmark.parser.Parser;
+import org.commonmark.renderer.html.HtmlRenderer;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 
 @Route(value = "about", layout = MainView.class)
 @PageTitle("About")
 @CssImport("./views/about/about-view.css")
 public class AboutView extends Div {
 
-    public AboutView() {
+    public AboutView() throws IOException {
         ThemeHelper.onLoad();
 
         addClassName("about-view");
-        add(new Text("Content placeholder"));
+
+        URL url = new URL("https://raw.githubusercontent.com/fabio-anzola/Helpupil/master/README.md");
+        URLConnection con = url.openConnection();
+        InputStream in = con.getInputStream();
+        String body = new String(in.readAllBytes());
+
+        Parser parser = Parser.builder().build();
+        Node document = parser.parse(body);
+        HtmlRenderer renderer = HtmlRenderer.builder().build();
+
+        Div div = new Div();
+        div.getElement().setProperty("innerHTML", renderer.render(document));
+
+        add(div);
     }
 
 }
