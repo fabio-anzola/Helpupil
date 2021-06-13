4ac932b831ccc4662261b4c7105d4ff27f72ca31
Tobias Rigler <8117@htl.rennweg.at>
2021-06-13 23:12:24 +0200

    Added JavaDoc for SecuredView

--- client/src/main/java/at/helpupil/application/utils/SecuredView.java
+++ client/src/main/java/at/helpupil/application/utils/SecuredView.java
@@ -2,7 +2,13 @@ package at.helpupil.application.utils;
 
 import com.vaadin.flow.component.html.Div;
 
+/**
+ * Only logged in users can see this view
+ */
 public class SecuredView extends Div {
+    /**
+     * redirects users to login if they aren't logged in
+     */
     public SecuredView() {
         super();
         Auth.redirectIfNotValid();
