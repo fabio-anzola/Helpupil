4940982c0194fb95a651960ce619ad28b5583151
Tobias Rigler <8117@htl.rennweg.at>
2021-06-13 20:07:50 +0200

    Optimized imports
    Removed possible Null-pointers in LeaderboardView

--- client/src/main/java/at/helpupil/application/views/leaderboard/LeaderboardView.java
+++ client/src/main/java/at/helpupil/application/views/leaderboard/LeaderboardView.java
@@ -3,15 +3,12 @@ package at.helpupil.application.views.leaderboard;
 import at.helpupil.application.utils.SecuredView;
 import at.helpupil.application.utils.SessionStorage;
 import at.helpupil.application.utils.ThemeHelper;
-import at.helpupil.application.utils.responses.Document;
 import at.helpupil.application.utils.responses.Error;
 import at.helpupil.application.utils.responses.LeaderboardObj;
 import at.helpupil.application.utils.responses.LeaderboardObjs;
 import at.helpupil.application.views.main.MainView;
 import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.grid.Grid;
-import com.vaadin.flow.component.html.Span;
-import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import kong.unirest.HttpResponse;
@@ -20,8 +17,6 @@ import kong.unirest.Unirest;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import static at.helpupil.application.Application.BASE_URL;
 
@@ -29,20 +24,20 @@ import static at.helpupil.application.Application.BASE_URL;
 @PageTitle("Leaderboard")
 @CssImport("./views/leaderboard/leaderboard-view.css")
 public class LeaderboardView extends SecuredView {
-    private Grid<LeaderboardObj> topUserGrid = new Grid<>(LeaderboardObj.class);
-    private LeaderboardObjs leaderboardObjs = makeLeaderboardRequest();
 
     public LeaderboardView() {
         ThemeHelper.onLoad();
 
         addClassName("leaderboard-view");
 
-
+        LeaderboardObjs leaderboardObjs = makeLeaderboardRequest();
+        if (leaderboardObjs == null) return;
         List<LeaderboardObj> leaderboardObjList = new ArrayList<>(Arrays.asList(leaderboardObjs.getResults()));
         for (int i = 0; i < leaderboardObjList.size(); i++) {
             leaderboardObjList.get(i).setPlace(i+1);
         }
 
+        Grid<LeaderboardObj> topUserGrid = new Grid<>(LeaderboardObj.class);
         topUserGrid.setColumns("place", "name", "wallet");
         topUserGrid.setItems(leaderboardObjList);
 
