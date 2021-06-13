0f98fa99de56a27b5c98714661b0febf4ec50b43
Tobias Rigler <8117@htl.rennweg.at>
2021-06-13 23:24:30 +0200

    Added JavaDoc for ThemeHelper

--- client/src/main/java/at/helpupil/application/utils/ThemeHelper.java
+++ client/src/main/java/at/helpupil/application/utils/ThemeHelper.java
@@ -5,8 +5,14 @@ import com.vaadin.flow.dom.ThemeList;
 import com.vaadin.flow.server.VaadinSession;
 import com.vaadin.flow.theme.lumo.Lumo;
 
+/**
+ * Helper class to switch between light and dark mode
+ */
 public class ThemeHelper {
 
+    /**
+     * reads from session if dark mode is preferred
+     */
     public static void onLoad() {
         ThemeList themeList = UI.getCurrent().getElement().getThemeList();
         if (readFromSession()) {
@@ -14,6 +20,9 @@ public class ThemeHelper {
         }
     }
 
+    /**
+     * activates/deactivates darkmode and saves it to session
+     */
     public static void onClick() {
         ThemeList themeList = UI.getCurrent().getElement().getThemeList();
 
@@ -26,10 +35,16 @@ public class ThemeHelper {
         }
     }
 
+    /**
+     * @param state saves state to session
+     */
     public static void setToSession(boolean state) {
         VaadinSession.getCurrent().setAttribute("darkmode", state);
     }
 
+    /**
+     * @return true if darkmode is set to true in session
+     */
     public static boolean readFromSession() {
         try {
             return (boolean) VaadinSession.getCurrent().getAttribute("darkmode");
