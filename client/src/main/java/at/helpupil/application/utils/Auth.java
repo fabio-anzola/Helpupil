69c914cd19fe039625f2e009a68b6800eb6ed185
Tobias Rigler <8117@htl.rennweg.at>
2021-06-13 22:58:49 +0200

    Added JavaDoc for Auth

--- client/src/main/java/at/helpupil/application/utils/Auth.java
+++ client/src/main/java/at/helpupil/application/utils/Auth.java
@@ -6,25 +6,39 @@ import com.vaadin.flow.server.VaadinServletRequest;
 
 import javax.servlet.http.HttpServletRequest;
 
+/**
+ * static methods to redirect the user
+ */
 public class Auth {
 
+    /**
+     * redirects user to login if session is null
+     */
     public static void redirectIfNotValid() {
         if (SessionStorage.isNull()) {
             UI.getCurrent().getPage().executeJs("window.location = \"" + getURL() + "/login\"");
         }
     }
 
+    /**
+     * redirects user to document view if user is valid
+     */
     public static void redirectIfValid() {
         if (!SessionStorage.isNull()) {
             UI.getCurrent().getPage().executeJs("window.location = \"" + getURL() + "/documents\"");
         }
     }
 
+    /**
+     * redirects user to login after register
+     */
     public static void redirectOnRegister() {
         UI.getCurrent().getPage().executeJs("window.location = \"" + getURL() + "/login\"");
-        // Notification.show("Please check your inbox");
     }
 
+    /**
+     * @return current url
+     */
     public static String getURL() {
         HttpServletRequest httpServletRequest = ((VaadinServletRequest) VaadinService.getCurrentRequest()).getHttpServletRequest();
         String[] requestUrl = httpServletRequest.getRequestURL().toString().split("/");
