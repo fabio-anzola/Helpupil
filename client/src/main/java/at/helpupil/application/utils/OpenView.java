b403f2a54cf63bfb6908b16c18ca12efd0a07140
Tobias Rigler <8117@htl.rennweg.at>
2021-06-13 23:01:32 +0200

    Added JavaDoc for OpenView

--- client/src/main/java/at/helpupil/application/utils/OpenView.java
+++ client/src/main/java/at/helpupil/application/utils/OpenView.java
@@ -2,7 +2,13 @@ package at.helpupil.application.utils;
 
 import com.vaadin.flow.component.html.Div;
 
+/**
+ * every user can see this view
+ */
 public class OpenView extends Div {
+    /**
+     * redirects user to documents view if he is logged in
+     */
     public OpenView() {
         super();
         Auth.redirectIfValid();
