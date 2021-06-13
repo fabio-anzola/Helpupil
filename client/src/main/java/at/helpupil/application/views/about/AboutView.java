14c60e69ea9a3e64bd83839d33150f9fa0ea3929
Anzola Fabio <anzola.fabio@gmail.com>
2021-06-13 19:37:44 +0200

    Error handling for about view

--- client/src/main/java/at/helpupil/application/views/about/AboutView.java
+++ client/src/main/java/at/helpupil/application/views/about/AboutView.java
@@ -1,19 +1,18 @@
 package at.helpupil.application.views.about;
 
 import at.helpupil.application.utils.ThemeHelper;
-import com.vaadin.flow.component.Text;
-import com.vaadin.flow.component.html.Div;
-import com.vaadin.flow.router.Route;
-import com.vaadin.flow.router.PageTitle;
 import at.helpupil.application.views.main.MainView;
+import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.dependency.CssImport;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
 import org.commonmark.node.Node;
 import org.commonmark.parser.Parser;
 import org.commonmark.renderer.html.HtmlRenderer;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 
@@ -22,24 +21,28 @@ import java.net.URLConnection;
 @CssImport("./views/about/about-view.css")
 public class AboutView extends Div {
 
-    public AboutView() throws IOException {
+    public AboutView() {
         ThemeHelper.onLoad();
 
         addClassName("about-view");
 
-        URL url = new URL("https://raw.githubusercontent.com/fabio-anzola/Helpupil/master/README.md");
-        URLConnection con = url.openConnection();
-        InputStream in = con.getInputStream();
-        String body = new String(in.readAllBytes());
+        try {
+            URL url = new URL("https://raw.githubusercontent.com/fabio-anzola/Helpupil/master/README.md");
+            URLConnection con = url.openConnection();
+            InputStream in = con.getInputStream();
+            String body = new String(in.readAllBytes());
 
-        Parser parser = Parser.builder().build();
-        Node document = parser.parse(body);
-        HtmlRenderer renderer = HtmlRenderer.builder().build();
+            Parser parser = Parser.builder().build();
+            Node document = parser.parse(body);
+            HtmlRenderer renderer = HtmlRenderer.builder().build();
 
-        Div div = new Div();
-        div.getElement().setProperty("innerHTML", renderer.render(document));
+            Div div = new Div();
+            div.getElement().setProperty("innerHTML", renderer.render(document));
 
-        add(div);
+            add(div);
+        } catch (IOException e) {
+            add(new Text("Readme could not be loaded"));
+        }
     }
 
 }
