7d1e9112f3ffc396037c23aee43483008d801c4f
Richard Krikler <8112@htl.rennweg.at>
2021-07-10 13:27:47 +0200

    Leaderboard-View: Added Podium for the top 3 Users

--- client/src/main/java/at/helpupil/application/views/leaderboard/LeaderboardView.java
+++ client/src/main/java/at/helpupil/application/views/leaderboard/LeaderboardView.java
@@ -9,6 +9,8 @@ import at.helpupil.application.utils.responses.LeaderboardObjs;
 import at.helpupil.application.views.main.MainView;
 import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import kong.unirest.HttpResponse;
@@ -36,23 +38,62 @@ public class LeaderboardView extends SecuredView {
 
         addClassName("leaderboard-view");
 
+
         LeaderboardObjs leaderboardObjs = makeLeaderboardRequest();
         if (leaderboardObjs == null) return;
         List<LeaderboardObj> leaderboardObjList = new ArrayList<>(Arrays.asList(leaderboardObjs.getResults()));
         for (int i = 0; i < leaderboardObjList.size(); i++) {
-            leaderboardObjList.get(i).setPlace(i+1);
+            leaderboardObjList.get(i).setPlace(i + 1);
         }
+
+
+        Div podiumLayout = new Div();
+        podiumLayout.addClassName("podium-layout");
+        if (leaderboardObjList.size() >= 2) {
+            podiumLayout.add(createPodiumPlaceLayout(leaderboardObjList.get(1).getName(), leaderboardObjList.get(1).getWallet()));
+        }
+        if (leaderboardObjList.size() >= 1) {
+            podiumLayout.add(createPodiumPlaceLayout(leaderboardObjList.get(0).getName(), leaderboardObjList.get(0).getWallet()));
+        }
+        if (leaderboardObjList.size() >= 3) {
+            podiumLayout.add(createPodiumPlaceLayout(leaderboardObjList.get(2).getName(), leaderboardObjList.get(2).getWallet()));
+        }
+        add(podiumLayout);
+
 
         Grid<LeaderboardObj> topUserGrid = new Grid<>(LeaderboardObj.class);
         topUserGrid.setColumns("place", "name", "wallet");
         topUserGrid.setItems(leaderboardObjList);
-
-
         add(topUserGrid);
     }
 
     /**
+     * Create a podium place
+     *
+     * @param username username of user
+     * @param wallet wallet value of user
+     * @return div podium place
+     */
+    private Div createPodiumPlaceLayout(String username, int wallet) {
+        Div podiumPlaceDiv = new Div();
+        podiumPlaceDiv.addClassName("podium-place-div");
+        Div podiumPlaceInnerDiv = new Div();
+        podiumPlaceInnerDiv.addClassName("podium-place-inner-div");
+
+        Div usernameDiv = new Div();
+        usernameDiv.add(new Span(username));
+
+        Div walletDiv = new Div();
+        walletDiv.add(new Span(Integer.toString(wallet)));
+
+        podiumPlaceInnerDiv.add(usernameDiv, walletDiv);
+        podiumPlaceDiv.add(podiumPlaceInnerDiv);
+        return podiumPlaceDiv;
+    }
+
+    /**
      * makes request to api to get the 10 users with the most points
+     *
      * @return 10 users with the most points
      */
     private LeaderboardObjs makeLeaderboardRequest() {
