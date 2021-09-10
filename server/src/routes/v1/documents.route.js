b1c61bd93595318e34dfe4e7dd2f0eb2e0a3dd91
Anzola Fabio <anzola.fabio@gmail.com>
2021-09-10 13:22:14 +0200

    Swagger docs for document search

--- server/src/routes/v1/documents.route.js
+++ server/src/routes/v1/documents.route.js
@@ -266,6 +266,81 @@ module.exports = router;
  *         $ref: '#/components/responses/Unauthorized'
  *       "403":
  *         $ref: '#/components/responses/Forbidden'
+ * /documents/search/:
+ *   get:
+ *     summary: Search for documents
+ *     description: Only logged in users can retrieve documents.
+ *     tags: [Documents]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: query
+ *         name: sensitive
+ *         schema:
+ *           type: boolean
+ *         description: Define if search is sensitive
+ *       - in: query
+ *         name: searchstring
+ *         schema:
+ *           type: string
+ *         description: The string to search for
+ *       - in: query
+ *         name: exact
+ *         schema:
+ *           type: boolean
+ *         description: Search for exactly the searchstring
+ *       - in: query
+ *         name: inverse
+ *         schema:
+ *           type: boolean
+ *         description: Inverse the search
+ *       - in: query
+ *         name: sortBy
+ *         schema:
+ *           type: string
+ *         description: sort by query in the form of field:desc/asc (ex. name:asc)
+ *       - in: query
+ *         name: limit
+ *         schema:
+ *           type: integer
+ *           minimum: 1
+ *         default: 10
+ *         description: Maximum number of users
+ *       - in: query
+ *         name: page
+ *         schema:
+ *           type: integer
+ *           minimum: 1
+ *           default: 1
+ *         description: Page number
+ *     responses:
+ *       "200":
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 results:
+ *                   type: array
+ *                   items:
+ *                     $ref: '#/components/schemas/Document'
+ *                 page:
+ *                   type: integer
+ *                   example: 1
+ *                 limit:
+ *                   type: integer
+ *                   example: 10
+ *                 totalPages:
+ *                   type: integer
+ *                   example: 1
+ *                 totalResults:
+ *                   type: integer
+ *                   example: 1
+ *       "401":
+ *         $ref: '#/components/responses/Unauthorized'
+ *       "403":
+ *         $ref: '#/components/responses/Forbidden'
  */
 
 /**
